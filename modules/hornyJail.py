f7106b099908ddeea3a66bfb7f361da1607bd0de
Matt Artist <11252574+mja00@users.noreply.github.com>
2021-04-15 22:03:50 -0400

    Adding week, month, and year to time convert (#1)

--- modules/hornyJail.py
+++ modules/hornyJail.py
@@ -50,7 +50,7 @@ def get_inmate(discord_id):
 
 
 def time_convert(time):
-    time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+    time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800, "M": 2592000, "y": 31536000}
     return int(time[:-1]) * time_convert[time[-1]]
 
 
